64fe97285c2da0052e58966afa7cc490f94d9a13
Vari-Cam <MC27593@westridge.org>
2026-04-30 10:48:34 -0700

    created event model

---
+++ controllers/eventController.js
@@ -0,0 +1,15 @@
+import Event from '../models/event.js'
+
+//EVENT FORM
+export const eventForm = async (req, res) => {
+  try {
+    const buildings = ("Science Building", "PAC", "Main Building", "Gym", "Library");
+    const sbRooms = ("SB70", "SB71", "SB72", "SB73");
+    res.render("index", { buildings });
+  } 
+  
+  catch (err) {
+    console.error(err);
+    res.status(500).send("Error loading form");
+  }
+};
