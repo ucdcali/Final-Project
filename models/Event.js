6442ff7eb6b906bde36346fbecdf0dded1d0b3c7
Vari-Cam <MC27593@westridge.org>
2026-05-08 10:09:11 -0700

    New Schema

--- models/Event.js
+++ models/Event.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
+const roomSchema = new mongoose.Schema({
+    rooms: [String],
+    buildings: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Building"
+    },
+});
+
 const eventSchema = new mongoose.Schema({
     event: String,
-    rooms: [{
-        location: String,
-        description: String,
-    }],
+    buildings: [roomSchema],
     description: String,
     id: String,
     creator: String,
