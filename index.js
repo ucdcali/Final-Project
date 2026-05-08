ce7711467e8aac6b9a47803b3ffe04c83a06e2f1
lilyc0817 <cockle4321@gmail.com>
2026-05-08 11:17:09 -0700

    map

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 dotenv.config();
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static("static"));
 
 // Routes
 app.use('/', router);
