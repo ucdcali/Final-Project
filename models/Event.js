5bf88fc76e1561ecc6216bae16a78e5ca7f953c4
lilyc0817 <cockle4321@gmail.com>
2026-05-14 10:29:45 -0700

    making different maps

--- models/Event.js
+++ models/Event.js
@@ -1,20 +1,15 @@
 import mongoose from "mongoose";
 
-const roomSchema = new mongoose.Schema({
-    rooms: [String],
-    buildings: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Building"
-    },
-});
-
 const eventSchema = new mongoose.Schema({
-    event: String,
-    buildings: [roomSchema],
-    description: String,
-    id: String,
-    creator: String,
-    time: String,
+  event: {
+    type: String,
+    required: true,
+  },
+  buildings: [String],
+  description: String,
+  id: String,
+  creator: String,
+  time: String,
 });
 
 export default mongoose.model("Event", eventSchema);
